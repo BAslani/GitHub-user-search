d4a8f3f93a4b8038d939baef2ca720af824f2910
Behzad Aslani <behzadaslani80@gmail.com>
2023-08-30 17:41:15 +0330

    feat(dashboard): displaying followers

--- src/components/Followers.js
+++ src/components/Followers.js
@@ -3,7 +3,27 @@ import { useGlobal } from '../context/context';
 import styled from 'styled-components';
 
 const Followers = () => {
-  return <h2>followers component</h2>;
+  const { followers } = useGlobal();
+
+  return <Wrapper>
+    <div className="followers">
+      {followers.map((follower) => {
+        const {
+          id,
+          login,
+          avatar_url: img,
+          html_url
+        } = follower;
+        return <article key={id}>
+          <img src={img} alt={login} />
+          <div>
+            <h4>{login}</h4>
+            <a href={html_url}>{html_url}</a>
+          </div>
+        </article>
+      })}
+    </div>
+  </Wrapper>;
 };
 
 const Wrapper = styled.article`
