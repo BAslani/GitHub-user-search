4df31f219700ee26dffb18f9d4f875087a9f0e61
Behzad Aslani <behzadaslani80@gmail.com>
2023-08-30 17:23:28 +0330

    feat(dashboard): displaying user info Card
    - avatar
    - profile link
    - bio
    - company
    - location
    - website

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,42 @@ import { useGlobal } from '../context/context';
 import styled from 'styled-components';
 import { MdBusiness, MdLocationOn, MdLink } from 'react-icons/md';
 const Card = () => {
-  return <h2>card component</h2>;
+  const {githubUser} = useGlobal();
+  const {
+    avatar_url,
+    html_url,
+    name,
+    company,
+    blog,
+    bio,
+    location,
+    twitter_username
+  } = githubUser;
+
+  return <Wrapper>
+    <header>
+      <img src={avatar_url} alt={name} />
+      <div>
+        <h4>{name}</h4>
+        <p>@{twitter_username || 'John doe'}</p>
+      </div>
+      <a href={html_url}>
+        follow
+      </a>
+    </header>
+    <p className="bio">
+      {bio}
+    </p>
+    <div className="links">
+      <p><MdBusiness/> {company}</p>
+    </div>
+    <div className="links">
+      <p><MdLocationOn/> {location}</p>
+    </div>
+    <div className="links">
+      <p><MdLink/> {blog}</p>
+    </div>
+  </Wrapper>
 };
 const Wrapper = styled.article`
   background: var(--clr-white);
