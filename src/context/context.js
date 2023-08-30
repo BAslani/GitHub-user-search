f6032121162f8f75577884357566e2e241aa2880
Behzad Aslani <behzadaslani80@gmail.com>
2023-08-30 12:23:59 +0330

    Build: context
    building context to lift up the states.

--- src/context/context.js
+++ src/context/context.js
@@ -5,3 +5,15 @@ import mockFollowers from './mockData.js/mockFollowers';
 import axios from 'axios';
 
 const rootUrl = 'https://api.github.com';
+
+const GithubContext = React.createContext();
+
+const GithubProvider = ({children}) => {
+    return <GithubContext.Provider value={'hi there'}>
+        {children}
+    </GithubContext.Provider>
+}
+
+const useGlobal = () => React.useContext(GithubContext);
+
+export {GithubProvider, useGlobal};
