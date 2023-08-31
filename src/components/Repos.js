aaeb7ac4ed2ea7f8b220f13b4470b681e3251053
Behzad Aslani <behzadaslani80@gmail.com>
2023-08-31 15:34:36 +0330

    feat(dashboard): popular langs & most forked
    display bar chart and column chart

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -44,28 +44,27 @@ const Repos = () => {
     return b.value - a.value;
   }).slice(0, 5);
 
-  // Chart Data
-  const chartData = [
-    {
-      label: "HTML",
-      value: "13"
-    },
-    {
-      label: "CSS",
-      value: "23"
-    },
-    {
-      label: "JavaScript",
-      value: "80"
-    }
-  ];
+
+  // stars, forks
+  let {stars, forks} = repos.reduce((total, item)=>{
+    const {stargazers_count, name, forks} = item;
+    total.stars[stargazers_count] = {label:name, value:stargazers_count}
+    total.forks[forks] = {label:name, value:forks}
+    return total;
+  },{
+    stars:{},
+    forks:{}
+  })
+
+  stars = Object.values(stars).slice(-5).reverse();
+  forks = Object.values(forks).slice(-5).reverse();
 
   return <section className="section">
     <Wrapper className='section-center'>
       <Pie3D data={languages} />
-      <Column3D data={chartData} />
+      <Column3D data={stars} />
       <Doughnut2D data={languageStars} />
-      <Bar3D data={chartData} />
+      <Bar3D data={forks} />
     </Wrapper>
   </section>
 };
