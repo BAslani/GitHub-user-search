e8e9e11f128f9beca8a8e635eb6d28387dac41de
Behzad Aslani <behzadaslani80@gmail.com>
2023-09-01 22:58:28 +0330

    feat: authentication setup

--- src/index.js
+++ src/index.js
@@ -11,9 +11,7 @@ root.render(
     <Auth0Provider
       domain={process.env.REACT_APP_DOMAIN}
       clientId={process.env.REACT_APP_CLIENT_ID}
-      authorizationParams={{
-        redirect_uri: window.location.origin
-      }}
+      redirectUri={window.location.origin}
     >
       <GithubProvider>
         <App />
